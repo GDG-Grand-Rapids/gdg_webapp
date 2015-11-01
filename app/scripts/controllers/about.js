a0e61e06cd082750367d08ab8c0dafe6b358aa57
Jeff Williams <Jeffrey.Williams2@spectrumhealth.org>
2015-11-01 15:10:30 -0500

    Used yeoman to create an angular webapp; used grunt/node to facilitate dependency management

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gdgWebappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gdgWebappApp
+ */
+angular.module('gdgWebappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gdgWebappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gdgWebappApp
+ */
+angular.module('gdgWebappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
