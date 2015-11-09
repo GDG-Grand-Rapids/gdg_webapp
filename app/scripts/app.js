e5a4399a935e7c5c6468e2b63a4a441a353b58ed
Jeff Williams <Jeffrey.Williams2@spectrumhealth.org>
2015-11-08 23:14:17 -0500

    Continued adding basic scaffold for the webapp

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,7 @@ angular.module('gdgWebappApp', [
     'ngMaterial',
     'ngRoute'
   ])
-  .controller('AppCtrl', function($scope, $location, $window) {
+  .controller('AppCtrl', function($document, $scope, $location, $window, $anchorScroll) {
     var self = this;
 
     self.hidden = false;
@@ -16,13 +16,43 @@ angular.module('gdgWebappApp', [
       icon: 'images/icons/twitter.svg',
       url: 'https://www.twitter.com/'
     }, {
+      name: 'Google Plus',
+      icon: 'images/icons/google-plus.svg',
+      url: 'https://plus.google.com/116106643420425488667/about'
+    }, {
+      name: 'Github',
+      icon: 'images/icons/github-circle.svg',
+      url: 'https://github.com/GDG-Grand-Rapids'
+    }, {
       name: 'Facebook',
       icon: 'images/icons/facebook.svg',
       url: 'https://www.facebook.com/gdggrandrapids'
     }];
 
-    $scope.redirect = function(url) {
-      $window.location.href = url;
+    $scope.header = 'big';
+    $scope.isHeaderMinimized = false;
+    angular.element($window).bind(
+      'scroll',
+      function() {
+        console.log($window.pageYOffset);
+        if ($window.pageYOffset > 0) {
+          $scope.header = 'small';
+          $scope.isHeaderMinimized = true;
+        } else {
+          $scope.header = 'big';
+          $scope.isHeaderMinimized = false;
+        }
+        $scope.opacity = function() {
+          var max = $document.innerHeight - $window.innerHeight;
+          var opacity = 1 - ($window.pageYOffset / max);
+          return opacity;
+        };
+        $scope.$apply();
+      });
+
+    $scope.gotoLocation = function() {
+      $location.hash('content');
+      $anchorScroll();
     };
 
     $scope.go = function(url) {
