c813c47b91c10f508d51dd17c06d5340c521505c
Jeff Williams <Jeffrey.Williams2@spectrumhealth.org>
2016-02-08 22:06:52 -0500

    Updated angular material version and fixed a bunch of stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,41 +4,46 @@ angular.module('gdgWebappApp', [
     'ngMaterial',
     'ngRoute'
   ])
-  .controller('AppCtrl', function ($document, $scope, $location, $window, $anchorScroll) {
+  .controller('AppCtrl', function($document, $scope, $location, $window, $anchorScroll) {
     var self = this;
 
-    self.items = [{
-      name: 'Twitter',
-      icon: 'images/icons/twitter.a59e6b76.svg',
-      url: 'https://www.twitter.com/'
-    }, {
-      name: 'Google Plus',
-      icon: 'images/icons/google-plus.8f170833.svg',
-      url: 'https://plus.google.com/116106643420425488667/about'
-    }, {
-      name: 'Github',
-      icon: 'images/icons/github-circle.231d7cad.svg',
-      url: 'https://github.com/GDG-Grand-Rapids'
-    }, {
-      name: 'Facebook',
-      icon: 'images/icons/facebook.c3e23151.svg',
-      url: 'https://www.facebook.com/gdggrandrapids'
-    }, {
-      name: 'Meetup',
-      icon: 'images/icons/calendar-check.09b470a8.svg',
-      url: 'http://www.meetup.com/GDG-Grand-Rapids/'
-    }];
-
-    $scope.gotoLocation = function () {
+    $scope.gotoLocation = function() {
       $location.hash('content');
       $anchorScroll();
     };
 
-    $scope.go = function (url) {
+    self.items = [{
+      name: 'Twitter',
+      // icon: 'images/icons/twitter.a59e6b76.svg',
+      icon: 'images/icons/twitter.svg',
+      url: 'https://www.twitter.com/'
+    }, {
+      name: 'Google Plus',
+      // icon: 'images/icons/google-plus.8f170833.svg',
+      icon: 'images/icons/google-plus.svg',
+      url: 'https://plus.google.com/116106643420425488667/about'
+    }, {
+      name: 'Github',
+      // icon: 'images/icons/github-circle.231d7cad.svg',
+      icon: 'images/icons/github-circle.svg',
+      url: 'https://github.com/GDG-Grand-Rapids'
+    }, {
+      name: 'Facebook',
+      // icon: 'images/icons/facebook.c3e23151.svg',
+      icon: 'images/icons/facebook.svg',
+      url: 'https://www.facebook.com/gdggrandrapids'
+    }, {
+      name: 'Meetup',
+      // icon: 'images/icons/calendar-check.09b470a8.svg',
+      icon: 'images/icons/calendar-check.svg',
+      url: 'http://www.meetup.com/GDG-Grand-Rapids/'
+    }];
+
+    $scope.go = function(url) {
       $location.path(url);
     };
   })
-  .config(function ($routeProvider) {
+  .config(function($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -54,6 +59,9 @@ angular.module('gdgWebappApp', [
         templateUrl: 'views/sponsors.html',
         controller: 'SponsorCtrl',
         controllerAs: 'sponsors'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contacts.html'
       })
       .otherwise({
         redirectTo: '/'
