1d9567f583df11e4af2163ea6b45d3b1be5f50b3
Jeff Williams <Jeffrey.Williams2@spectrumhealth.org>
2016-03-12 17:53:16 -0500

    Finalizing the site

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,54 +4,27 @@ angular.module('gdgWebappApp', [
     'ngMaterial',
     'ngRoute'
   ])
-  .controller('AppCtrl', function($scope, $location, $window, $timeout, $mdSidenav) {
-    function buildToggler(navID) {
-      return function() {
-        $mdSidenav(navID).toggle();
-      };
-    }
-    $scope.toggleRight = buildToggler('right');
-    $scope.isOpenRight = function(){
-      return $mdSidenav('right').isOpen();
-    };
-    $scope.close = function () {
-      $mdSidenav('right').close();
-    };
-
-    $scope.go = function(url) {
-      $location.path(url);
-    };
-
-    $scope.scrollToTop = function() {
-      $window.scrollTo(0, 0);
-    };
-
-    this.items = [{
-      name: 'Twitter',
-      // icon: 'images/icons/twitter.a59e6b76.svg',
-      icon: 'images/icons/twitter.svg',
-      url: 'https://twitter.com/ioextendedgr'
-    }, {
-      name: 'Google Plus',
-      // icon: 'images/icons/google-plus.8f170833.svg',
-      icon: 'images/icons/google-plus.svg',
-      url: 'https://plus.google.com/116106643420425488667/about'
-    }, {
-      name: 'Github',
-      // icon: 'images/icons/github-circle.231d7cad.svg',
-      icon: 'images/icons/github-circle.svg',
-      url: 'https://github.com/GDG-Grand-Rapids'
-    }, {
-      name: 'Facebook',
-      // icon: 'images/icons/facebook.c3e23151.svg',
-      icon: 'images/icons/facebook.svg',
-      url: 'https://www.facebook.com/gdggrandrapids'
-    }, {
-      name: 'Meetup',
-      // icon: 'images/icons/calendar-check.09b470a8.svg',
-      icon: 'images/icons/calendar-check.svg',
-      url: 'http://www.meetup.com/GDG-Grand-Rapids/'
-    }];
+  .controller('AppCtrl', function() {
+    // function buildToggler(navID) {
+    //   return function() {
+    //     $mdSidenav(navID).toggle();
+    //   };
+    // }
+    // $scope.toggleRight = buildToggler('right');
+    // $scope.isOpenRight = function(){
+    //   return $mdSidenav('right').isOpen();
+    // };
+    // $scope.close = function () {
+    //   $mdSidenav('right').close();
+    // };
+    //
+    // $scope.go = function(url) {
+    //   $location.path(url);
+    // };
+    //
+    // $scope.scrollToTop = function() {
+    //   $window.scrollTo(0, 0);
+    // };
   })
   .config(function($routeProvider, $mdThemingProvider) {
     $mdThemingProvider.definePalette('gdgPalette', {
@@ -72,56 +45,39 @@ angular.module('gdgWebappApp', [
     });
     $mdThemingProvider.theme('default')
       .primaryPalette('gdgPalette', {
-        'default': '700',
+        'default': 'A100',
         'hue-1': '50',
         'hue-2': '50',
         'hue-3': '50'
       });
 
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/home.html',
-        controller: 'HomeCtrl',
-        controllerAs: 'home'
-      })
-      .when('/events', {
-        templateUrl: 'views/events.html',
-        controller: 'EventsCtrl',
-        controllerAs: 'events'
-      })
-      .when('/sponsors', {
-        templateUrl: 'views/sponsors.html',
-        controller: 'SponsorCtrl',
-        controllerAs: 'sponsors'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .when('/contact', {
-        templateUrl: 'views/contact.html',
-        controller: 'ContactCtrl',
-        controllerAs: 'contact'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+    // $routeProvider
+    //   .when('/', {
+    //     templateUrl: 'views/home.html',
+    //     controller: 'HomeCtrl',
+    //     controllerAs: 'home'
+    //   })
+    //   .when('/events', {
+    //     templateUrl: 'views/events.html',
+    //     controller: 'EventsCtrl',
+    //     controllerAs: 'events'
+    //   })
+    //   .when('/sponsors', {
+    //     templateUrl: 'views/sponsors.html',
+    //     controller: 'SponsorCtrl',
+    //     controllerAs: 'sponsors'
+    //   })
+    //   .when('/about', {
+    //     templateUrl: 'views/about.html',
+    //     controller: 'AboutCtrl',
+    //     controllerAs: 'about'
+    //   })
+    //   .when('/contact', {
+    //     templateUrl: 'views/contact.html',
+    //     controller: 'ContactCtrl',
+    //     controllerAs: 'contact'
+    //   })
+    //   .otherwise({
+    //     redirectTo: '/'
+    //   });
   });
-
-// app.directive('scroll', function($window) {
-//   return function($scope) {
-//     angular.element($window).bind('scroll', function() {
-//       if (this.pageYOffset >= 440) {
-//         $scope.menuScroll = true;
-//       } else {
-//         if ($scope.home === false) {
-//           $scope.menuScroll = true;
-//         } else {
-//           $scope.menuScroll = false;
-//         }
-//       }
-//       $scope.$apply();
-//     });
-//   };
-// });
---
+++ app/scripts/countdownclock.js
@@ -0,0 +1,49 @@
+'use strict';
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+function parseDate(input) {
+  var parts = input.split('-');
+  // new Date(year, month [, day [, hours[, minutes[, seconds[, ms]]]]])
+  return new Date(parts[0], parts[1] - 1, parts[2]); // Note: months are 0-based
+}
+
+var deadline = new Date(parseDate('2016-05-18') + 15 * 24 * 60 * 60 * 1000);
+initializeClock('gdg-CountDownClock', deadline);
