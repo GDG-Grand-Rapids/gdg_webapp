682bfafdffd24987132496ea2413b40b8b71393a
Jeff Williams <Jeffrey.Williams2@spectrumhealth.org>
2016-03-12 10:19:01 -0500

    Adding different theme to site

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,90 +1,78 @@
 'use strict';
 
-var app = angular.module('gdgWebappApp', [
+angular.module('gdgWebappApp', [
     'ngMaterial',
     'ngRoute'
   ])
-  .controller('AppCtrl', function($scope, $location, $window, $timeout, $mdSidenav, $log) {
+  .controller('AppCtrl', function($scope, $location, $window, $timeout, $mdSidenav) {
+    function buildToggler(navID) {
+      return function() {
+        $mdSidenav(navID).toggle();
+      };
+    }
     $scope.toggleRight = buildToggler('right');
     $scope.isOpenRight = function(){
       return $mdSidenav('right').isOpen();
     };
-    function buildToggler(navID) {
-      return function() {
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {
-            $log.debug("toggle " + navID + " is done");
-          });
-      }
-    }
-
     $scope.close = function () {
-      $mdSidenav('right').close()
-        .then(function () {
-          $log.debug("close RIGHT is done");
-        });
+      $mdSidenav('right').close();
     };
 
     $scope.go = function(url) {
       $location.path(url);
     };
 
-    $scope.scrollToTop = function(home) {
-      if (home === 'home') {
-        $window.scrollTo(0, 0);
-      } else {
-        $window.scrollTo(0, 1);
-      }
+    $scope.scrollToTop = function() {
+      $window.scrollTo(0, 0);
     };
 
     this.items = [{
       name: 'Twitter',
-      icon: 'images/icons/twitter.a59e6b76.svg',
-      // icon: 'images/icons/twitter.svg',
+      // icon: 'images/icons/twitter.a59e6b76.svg',
+      icon: 'images/icons/twitter.svg',
       url: 'https://twitter.com/ioextendedgr'
     }, {
       name: 'Google Plus',
-      icon: 'images/icons/google-plus.8f170833.svg',
-      // icon: 'images/icons/google-plus.svg',
+      // icon: 'images/icons/google-plus.8f170833.svg',
+      icon: 'images/icons/google-plus.svg',
       url: 'https://plus.google.com/116106643420425488667/about'
     }, {
       name: 'Github',
-      icon: 'images/icons/github-circle.231d7cad.svg',
-      // icon: 'images/icons/github-circle.svg',
+      // icon: 'images/icons/github-circle.231d7cad.svg',
+      icon: 'images/icons/github-circle.svg',
       url: 'https://github.com/GDG-Grand-Rapids'
     }, {
       name: 'Facebook',
-      icon: 'images/icons/facebook.c3e23151.svg',
-      // icon: 'images/icons/facebook.svg',
+      // icon: 'images/icons/facebook.c3e23151.svg',
+      icon: 'images/icons/facebook.svg',
       url: 'https://www.facebook.com/gdggrandrapids'
     }, {
       name: 'Meetup',
-      icon: 'images/icons/calendar-check.09b470a8.svg',
-      // icon: 'images/icons/calendar-check.svg',
+      // icon: 'images/icons/calendar-check.09b470a8.svg',
+      icon: 'images/icons/calendar-check.svg',
       url: 'http://www.meetup.com/GDG-Grand-Rapids/'
     }];
   })
   .config(function($routeProvider, $mdThemingProvider) {
     $mdThemingProvider.definePalette('gdgPalette', {
-      '50': 'ffffff',
-      '100': 'B2DFDB',
-      '200': '80CBC4',
-      '300': '4DB6AC',
-      '400': '26A69A',
-      '500': '006064',
-      '600': '00897B',
-      '700': '00796B',
-      '800': '00695C',
-      '900': '004D40',
-      'A100': 'A7FFEB',
-      'A200': '64FFDA',
-      'A400': '1DE9B6',
-      'A700': '00BFA5'
+      '50': 'ECEFF1',
+      '100': 'CFD8DC',
+      '200': 'B0BEC5',
+      '300': '90A4AE',
+      '400': '78909C',
+      '500': '607D8B',
+      '600': '546E7A',
+      '700': '455A64',
+      '800': '37474F',
+      '900': '263238',
+      'A100': 'FFFFFF',
+      'A200': 'FFFFFF',
+      'A400': 'FFFFFF',
+      'A700': 'FFFFFF'
     });
     $mdThemingProvider.theme('default')
       .primaryPalette('gdgPalette', {
-        'default': '500',
+        'default': '700',
         'hue-1': '50',
         'hue-2': '50',
         'hue-3': '50'
@@ -121,19 +109,19 @@ var app = angular.module('gdgWebappApp', [
       });
   });
 
-app.directive('scroll', function($window) {
-  return function($scope) {
-    angular.element($window).bind('scroll', function() {
-      if (this.pageYOffset >= 1) {
-        $scope.menuScroll = true;
-      } else {
-        if ($scope.home === false) {
-          $scope.menuScroll = true;
-        } else {
-          $scope.menuScroll = false;
-        }
-      }
-      $scope.$apply();
-    });
-  };
-});
+// app.directive('scroll', function($window) {
+//   return function($scope) {
+//     angular.element($window).bind('scroll', function() {
+//       if (this.pageYOffset >= 440) {
+//         $scope.menuScroll = true;
+//       } else {
+//         if ($scope.home === false) {
+//           $scope.menuScroll = true;
+//         } else {
+//           $scope.menuScroll = false;
+//         }
+//       }
+//       $scope.$apply();
+//     });
+//   };
+// });
